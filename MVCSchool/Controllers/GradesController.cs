cbb6796ee16565875a0727e26d47eb21266bb94a
Dusan <dusan.kovarik@outlook.com>
2025-12-02 00:57:30 +0100

    Add Create Grade functionality

--- MVCSchool/Controllers/GradesController.cs
+++ MVCSchool/Controllers/GradesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using MVCSchool.DTO;
 using MVCSchool.Services;
 
 namespace MVCSchool.Controllers {
@@ -15,10 +16,16 @@ namespace MVCSchool.Controllers {
         }
 
         public async Task<IActionResult> CreateAsync() {
-            var gradeDropdownsData = await _service.GetNewGradeDropdownsValues();
+            var gradeDropdownsData = await _service.GetNewGradeDropdownsValuesAsync();
             ViewBag.Students = new SelectList(gradeDropdownsData.Students, "Id", "FullName");
             ViewBag.Subjects = new SelectList(gradeDropdownsData.Subjects, "Id", "Name");
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(GradeDTO newGrade) {
+            await _service.CreateAsync(newGrade);
+            return RedirectToAction("Index");
         }
     }
 }
--- MVCSchool/Services/GradesService.cs
+++ MVCSchool/Services/GradesService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MVCSchool.DTO;
 using MVCSchool.Models;
 using MVCSchool.ViewModels;
 
@@ -10,12 +11,35 @@ namespace MVCSchool.Services {
             _dbContext = dbContext;
         }
 
-        public async Task<GradesDropdownsViewModel> GetNewGradeDropdownsValues() {
+        public async Task<GradesDropdownsViewModel> GetNewGradeDropdownsValuesAsync() {
             var gradeDropdownsData = new GradesDropdownsViewModel() {
                 Students = await _dbContext.Students.OrderBy(st => st.LastName).ToListAsync(),
                 Subjects = await _dbContext.Subjects.OrderBy(sub => sub.Name).ToListAsync(),
             };
             return gradeDropdownsData;
+        }
+
+        public async Task CreateAsync(GradeDTO newGrade) {
+            Grade gradeToInsert = DtoToModel(newGrade);
+            if (gradeToInsert != null) {
+                await _dbContext.Grades.AddAsync(gradeToInsert);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+
+        private Grade DtoToModel(GradeDTO gradeDto) {
+            var student = _dbContext.Students.FirstOrDefault(st => st.Id == gradeDto.StudentId);
+            var subject = _dbContext.Subjects.FirstOrDefault(sub => sub.Id == gradeDto.SubjectId);
+            if (student == null || subject == null) {
+                return null!;
+            }
+            return new Grade {
+                Student = student,
+                Subject = subject,
+                Date = DateTime.Now,
+                Topic = gradeDto.Topic,
+                Mark = gradeDto.Mark,
+            };
         }
     }
 }
