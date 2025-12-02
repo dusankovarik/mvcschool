b9df46af40a6aeb292e602c6cdfe22f97cbcbd63
Dusan <dusan.kovarik@outlook.com>
2025-12-02 23:22:50 +0100

    Improve displaying Edit Grade View

--- MVCSchool/DTO/GradeDTO.cs
+++ MVCSchool/DTO/GradeDTO.cs
@@ -1,8 +1,15 @@
-﻿namespace MVCSchool.DTO {
+﻿using System.ComponentModel;
+
+namespace MVCSchool.DTO {
     public class GradeDTO {
         public int Id { get; set; }
+
+        [DisplayName("Student Name")]
         public int StudentId { get; set; }
+
+        [DisplayName("Subject Name")]
         public int SubjectId { get; set; }
+
         public required string Topic { get; set; }
         public int Mark { get; set; }
         public DateTime Date { get; set; }
