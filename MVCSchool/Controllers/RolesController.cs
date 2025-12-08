eff27b4d11e6787f010f66576e86f2c0d2cdc66b
Dusan <dusan.kovarik@outlook.com>
2025-12-08 19:42:22 +0100

    Add Roles Index View

---
+++ MVCSchool/Controllers/RolesController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCSchool.Controllers {
+    public class RolesController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
